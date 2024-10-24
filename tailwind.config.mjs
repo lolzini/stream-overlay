1ef6fc186577595b029fbecce651b517a73e8df2
lolzini <97997498+lolzini@users.noreply.github.com>
2024-10-23 18:48:18 -0600

    add: ending screen

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,9 @@ export default {
         ".custom-shadow": {
           filter: `drop-shadow(2px 2px 0px #000000);`,
         },
+        ".custom-shadow-xl": {
+          filter: `drop-shadow(0.5rem 0.5rem 0px #000000);`,
+        },
       });
     },
   ],
